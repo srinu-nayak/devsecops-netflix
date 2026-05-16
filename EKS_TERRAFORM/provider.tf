45fc6ab9193bd5e930bccdb9a472553f802cbee4
Srinu Karamtoth <srinuengr@gmail.com>
2026-05-15 21:59:58 -0400

    Upgrade AWS provider to version 6.0 and change region
    Updated AWS provider version and region in provider.tf

--- EKS_TERRAFORM/provider.tf
+++ EKS_TERRAFORM/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-2"
 }
